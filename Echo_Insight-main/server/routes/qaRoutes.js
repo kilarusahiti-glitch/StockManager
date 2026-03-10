1abcf94f045a1c59c4656575b9b1d9b621738b15
kilarusahiti-glitch <kilarusahiti@gmail.com>
2026-03-10 21:32:13 +0530

    Delete Echo_Insight-main directory

--- Echo_Insight-main/server/routes/qaRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { askQuestionController } from "../controllers/qaController.js";
-import protect from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-router.post("/ask", protect, askQuestionController);
-export default router;
--- Echo_Insight-main/server/routes/ttsRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import protect from "../middleware/authMiddleware.js";
-import { generateTTS } from "../controllers/ttsController.js";
-
-const router = express.Router();
-router.post("/generate", protect, generateTTS);
-export default router;
